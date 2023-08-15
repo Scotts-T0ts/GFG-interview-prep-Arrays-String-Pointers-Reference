ebac73cf7f07d17acdbb2f43860fb627836deb78
Neelay Yadav <103605666+Scotts-T0ts@users.noreply.github.com>
2023-08-15 09:38:17 +0530

    Add files via upload

---
+++ 8_reference_swap.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+void
+swap (int &x, int &y)                  //pass by reference
+{
+  int temp = x;
+  x = y;
+  y = temp;
+}
+
+
+int
+main ()
+{
+
+  int x = 10, y = 20;
+  swap (x, y);
+  cout << x << " " << y;
+  return 0;
+}
